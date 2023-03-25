f8b962cbcf0b0afdf4dc835b79b239436d71790d
Tom Campbell <tyen901@gmail.com>
2023-03-25 14:41:31 +1300

    Custom colors

--- tnc_uniforms/config.cpp
+++ tnc_uniforms/config.cpp
@@ -7,13 +7,22 @@ class CfgPatches
 		author = "Tyen";
 		requiredAddons[] = {"A3_Characters_F","A3_Data_F","A3_Soft_F"};
 		units[] = {};
-		weapons[] = {"TNC_CUP_CRYE_V1_Full"};
+		weapons[] =
+		{
+			"TNC_U_CRYE_G3_M81_WHITE",
+			"TNC_U_CRYE_G3_M81_RED",
+			"TNC_U_CRYE_G3_M81_GREEN",
+			"TNC_U_CRYE_G3_M81_BLUE",
+			"TNC_U_CRYE_G3_M81_YELLOW",
+			"TNC_U_CRYE_G3_M81_PURPLE",
+			"TNC_U_CRYE_G3_M81_ORANGE",
+		};
 	};
 };
 class CfgVehicles
 {
 	class CUP_B_USArmy_Soldier_01;
-	class TNC_CUP_CRYE_V1_Full: CUP_B_USArmy_Soldier_01
+	class TNC_CRYE_M81_BASE: CUP_B_USArmy_Soldier_01
 	{
 		author = "Tyen";
 		dlc = "TNC_uniforms";
@@ -21,10 +30,37 @@ class CfgVehicles
 		scopeCurator = 0;
 		displayName = "Soldier";
 		model = "\A3\characters_F\BLUFOR\b_soldier_01.p3d";
-		uniformClass = "TNC_CUP_CRYE_V1_Full";
 		hiddenSelections[] = {"Camo","insignia"};
 		hiddenSelectionsTextures[] = {"\CUP\Creatures\People\Military\CUP_Creatures_People_Military_USarmy\data\delta\crye_mcam_v1_co.paa"};
 		hiddenSelectionsMaterials[] = {"\CUP\Creatures\People\Military\CUP_Creatures_People_Military_USarmy\data\delta\crye_g3_mcam_v1.rvmat"};
+	};
+	class TNC_CRYE_WHITE: TNC_CRYE_M81_BASE
+	{
+		uniformClass = "TNC_U_CRYE_G3_M81_WHITE";
+	};
+	class TNC_CRYE_RED: TNC_CRYE_M81_BASE
+	{
+		uniformClass = "TNC_U_CRYE_G3_M81_RED";
+	};
+	class TNC_CRYE_GREEN: TNC_CRYE_M81_BASE
+	{
+		uniformClass = "TNC_U_CRYE_G3_M81_GREEN";
+	};
+	class TNC_CRYE_BLUE: TNC_CRYE_M81_BASE
+	{
+		uniformClass = "TNC_U_CRYE_G3_M81_BLUE";
+	};
+	class TNC_CRYE_YELLOW: TNC_CRYE_M81_BASE
+	{
+		uniformClass = "TNC_U_CRYE_G3_M81_YELLOW";
+	};
+	class TNC_CRYE_PURPLE: TNC_CRYE_M81_BASE
+	{
+		uniformClass = "TNC_U_CRYE_G3_M81_PURPLE";
+	};
+	class TNC_CRYE_ORANGE: TNC_CRYE_M81_BASE
+	{
+		uniformClass = "TNC_U_CRYE_G3_M81_ORANGE";
 	};
 };
 class CfgWeapons
@@ -33,23 +69,82 @@ class CfgWeapons
 	class CUP_U_B_US_BDU;
 
 	//Crye G3 Uniforms
-	class CUP_U_CRYE_V1_Full: CUP_U_B_US_BDU
+	class TNC_U_CRYE_G3_M81_BASE: CUP_U_B_US_BDU
 	{
 		author = "Tyen";
-		dlc = "TNC_uniforms";
 		scope = 2;
 		picture = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_USArmy\data\ui\icon_CUP_CRYE_V1_Full.paa";
-		displayName = "Crye AAAAAAAAAA Full";
+		displayName = "Crye M81 Base";
 		allowedSlots[] = {901};
 		model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
 		hiddenSelections[] = {"Camo","insignia"};
 		hiddenSelectionsTextures[] = {"CUP\Creatures\People\Military\CUP_Creatures_People_Military_USarmy\data\delta\crye_mcam_v1_co.paa"};
 		class ItemInfo: UniformItem
 		{
-			uniformModel = "-";
-			uniformClass = "TNC_CUP_CRYE_V1_Full";
-			containerClass = "Supply40";
-			mass = 20;
+			uniformClass = "TNC_CRYE_M81_BASE";
+		};
+	};
+	class TNC_U_CRYE_G3_M81_WHITE: TNC_U_CRYE_G3_M81_BASE
+	{
+		displayName = "Crye M81 White";
+		hiddenSelectionsTextures[] = {"TNC\tnc_uniforms\data\uniforms\cyre_g3_m81_white_diffuse.paa"};
+		class ItemInfo: UniformItem
+		{
+			uniformClass = "TNC_CRYE_WHITE";
+		};
+	};
+	class TNC_U_CRYE_G3_M81_RED: TNC_U_CRYE_G3_M81_BASE
+	{
+		displayName = "Crye M81 Red";
+		hiddenSelectionsTextures[] = {"TNC\tnc_uniforms\data\uniforms\cyre_g3_m81_red_diffuse.paa"};
+		class ItemInfo: UniformItem
+		{
+			uniformClass = "TNC_CRYE_RED";
+		};
+	};
+	class TNC_U_CRYE_G3_M81_GREEN: TNC_U_CRYE_G3_M81_BASE
+	{
+		displayName = "Crye M81 Green";
+		hiddenSelectionsTextures[] = {"TNC\tnc_uniforms\data\uniforms\cyre_g3_m81_green_diffuse.paa"};
+		class ItemInfo: UniformItem
+		{
+			uniformClass = "TNC_CRYE_GREEN";
+		};
+	};
+	class TNC_U_CRYE_G3_M81_BLUE: TNC_U_CRYE_G3_M81_BASE
+	{
+		displayName = "Crye M81 Blue";
+		hiddenSelectionsTextures[] = {"TNC\tnc_uniforms\data\uniforms\cyre_g3_m81_blue_diffuse.paa"};
+		class ItemInfo: UniformItem
+		{
+			uniformClass = "TNC_CRYE_BLUE";
+		};
+	};
+	class TNC_U_CRYE_G3_M81_YELLOW: TNC_U_CRYE_G3_M81_BASE
+	{
+		displayName = "Crye M81 Yellow";
+		hiddenSelectionsTextures[] = {"TNC\tnc_uniforms\data\uniforms\cyre_g3_m81_yellow_diffuse.paa"};
+		class ItemInfo: UniformItem
+		{
+			uniformClass = "TNC_CRYE_YELLOW";
+		};
+	};
+	class TNC_U_CRYE_G3_M81_PURPLE: TNC_U_CRYE_G3_M81_BASE
+	{
+		displayName = "Crye M81 Purple";
+		hiddenSelectionsTextures[] = {"TNC\tnc_uniforms\data\uniforms\cyre_g3_m81_purple_diffuse.paa"};
+		class ItemInfo: UniformItem
+		{
+			uniformClass = "TNC_CRYE_PURPLE";
+		};
+	};
+	class TNC_U_CRYE_G3_M81_ORANGE: TNC_U_CRYE_G3_M81_BASE
+	{
+		displayName = "Crye M81 Orange";
+		hiddenSelectionsTextures[] = {"TNC\tnc_uniforms\data\uniforms\cyre_g3_m81_orange_diffuse.paa"};
+		class ItemInfo: UniformItem
+		{
+			uniformClass = "TNC_CRYE_ORANGE";
 		};
 	};
 };
