44a50fb1a302eb7af0908bf2981efef31a375634
Tom Campbell <tyen901@gmail.com>
2023-09-16 18:45:05 +1200

    Add TNC_Shared with clean eo_glass_ca texture

---
+++ tnc_shared/config.cpp
@@ -0,0 +1,12 @@
+#define _ARMA_
+
+class CfgPatches
+{
+    class TNC_Shared
+    {
+        author = "Tyen";
+        requiredAddons[] = {"A3_Characters_F", "A3_Data_F", "A3_Soft_F"};
+        units[] = { };
+        weapons[] = { };
+    };
+};
