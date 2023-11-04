ebcbf6835b4bc485d0fa5e669e3d6a0af99646ee
Tom Campbell <tyen901@gmail.com>
2023-11-04 21:02:40 +1300

    Rename tnc_headgear_pumpkin to headgear_pumpkin

--- headgear_pumpkin/config.cpp
+++ headgear_pumpkin/config.cpp
@@ -2,11 +2,11 @@
 
 class CfgPatches
 {
-    class TNC_GEAR
+    class TC_GEAR
     {
         units[] = {};
         weapons[] = {
-            "TNC_NVG_Hat_Pumpkin",
+            "TC_NVG_Hat_Pumpkin",
             };
         requiredVersion = 0.1;
         requiredAddons[] = {"A3_Characters_F", "A3_Data_F", "A3_Soft_F", "A3_Props_F_Decade_Humanitarian"};
@@ -15,21 +15,21 @@ class CfgPatches
 class CfgWeapons
 {
     class NVGoggles;
-    class TNC_NVG_Hat_Pumpkin: NVGoggles
+    class TC_NVG_Hat_Pumpkin: NVGoggles
     {
         scope = 2;
         author = "Tyen";
         displayName = "Pumpkin Hat (Halloween)";
         descriptionShort = "Pumpkin, Halloween";
-        model = "\TNC\tnc_headwear_pumpkin\hat_pumpkin.p3d";
-        modelOptics = "\A3\Weapons_F\empty";
+        model = "/TC/headgear_pumpkin/pumpkin_halloween.p3d";
+        modelOptics = "/A3/Weapons_F/empty";
         visionMode[] = {"Normal","Normal"};
         class ItemInfo
         {
             type = 616;
             hmdType = 0;
-            uniformModel = "\TNC\tnc_headwear_pumpkin\hat_pumpkin.p3d";
-            modelOff = "\TNC\tnc_headwear_pumpkin\hat_pumpkin.p3d";
+            uniformModel = "/TC/headgear_pumpkin/pumpkin_halloween.p3d";
+            modelOff = "/TC/headgear_pumpkin/pumpkin_halloween.p3d";
             mass = 5;
         };
     };
@@ -37,13 +37,13 @@ class CfgWeapons
 class CfgGlasses
 {
     class None;
-    class TNC_G_Hat_Pumpkin: None
+    class TC_G_Hat_Pumpkin: None
     {
         scope = 2;
         author = "Tyen";
         displayName = "Pumpkin Hat (Halloween)";
         descriptionShort = "Pumpkin, Halloween";
-        model = "\TNC\tnc_headwear_pumpkin\hat_pumpkin.p3d";
+        model = "/TC/headgear_pumpkin/pumpkin_halloween.p3d";
         identityTypes[] = {};
         mass = 5;
     };
